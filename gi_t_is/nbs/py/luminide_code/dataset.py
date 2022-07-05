c51fee804a707f7152a1bd45ecc4d9917b01c977
diwert-ai <diwert08@gmail.com>
2022-07-05 20:35:27 +0300

    commit gi_t_is

---
+++ gi_t_is/nbs/py/luminide_code/dataset.py
@@ -0,0 +1,76 @@
+import os
+import cv2
+import numpy as np
+import torch.utils.data as data
+
+
+class VisionDataset(data.Dataset):
+    def __init__(
+            self, df, conf, input_dir, imgs_dir,
+            class_names, transform, is_test=False, subset=100):
+        self.conf = conf
+        self.transform = transform
+        self.is_test = is_test
+
+        if subset != 100:
+            assert subset < 100
+            # train and validate on subsets
+            num_rows = df.shape[0]*subset//100
+            df = df.iloc[:num_rows]
+
+        files = df['img_files']
+        self.files = [os.path.join(input_dir, imgs_dir, f) for f in files]
+        self.masks = [os.path.join(input_dir, 'masks', f) for f in files]
+
+    def resize(self, img, interp):
+        #print(f'resize: {img.shape}')
+        return  cv2.resize(
+            img, (self.conf.w_image_size,self.conf.h_image_size), interpolation=interp)
+
+    def load_slice(self, img_file, diff):
+        slice_num = os.path.basename(img_file).split('_')[1]
+        filename = (
+            img_file.replace(
+                'slice_' + slice_num,
+                'slice_' + str(int(slice_num) + diff).zfill(4)))
+        if os.path.exists(filename):
+            return cv2.imread(filename, cv2.IMREAD_UNCHANGED)
+        return None
+
+    def __getitem__(self, index):
+        conf = self.conf
+        img_file = self.files[index]
+        # read 5 slices into one image
+        imgs = [self.load_slice(img_file, i) for i in range(-2, 3)]
+        if imgs[3] is None:
+            imgs[3] = imgs[2]
+        if imgs[4] is None:
+            imgs[4] = imgs[3]
+        if imgs[1] is None:
+            imgs[1] = imgs[2]
+        if imgs[0] is None:
+            imgs[0] = imgs[1]
+        img = np.stack(imgs, axis=2)
+
+        img = img.astype(np.float32)
+        max_val = img.max()
+        if max_val != 0:
+            img /= max_val
+        img = self.resize(img, cv2.INTER_AREA)
+
+        if self.is_test:
+            msk = 0
+            result = self.transform(image=img)
+            img = result['image']
+        else:
+            # read mask
+            msk_file = self.masks[index]
+            msk = cv2.imread(msk_file, cv2.IMREAD_UNCHANGED)
+            msk = self.resize(msk, cv2.INTER_NEAREST)
+            msk = msk.astype(np.float32)
+            result = self.transform(image=img, mask=msk)
+            img, msk = result['image'], result['mask']
+        return img, msk
+
+    def __len__(self):
+        return len(self.files)
---
+++ gi_t_is/nbs/py/luminide_code/util.py
@@ -0,0 +1,143 @@
+import os
+from glob import glob
+import cv2
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from matplotlib import pyplot as plt
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from tqdm.notebook import tqdm
+
+class LossHistory:
+    def __init__(self, save_dir=None):
+        self.data = []
+        self.save_dir = save_dir
+        self.hist_filename=f"history_{datetime.now().strftime('%b%d_%H-%M-%S')}.csv"
+
+    def add_val_loss(self, epoch, sample_count, val):
+        self.data.append(
+            [len(self.data), epoch, sample_count, np.nan, val, np.nan])
+
+    def add_train_loss(self, epoch, sample_count, val):
+        self.data.append(
+            [len(self.data), epoch, sample_count, val, np.nan, np.nan])
+
+    def add_epoch_val_loss(self, epoch, sample_count, val):
+        self.data.append(
+            [len(self.data), epoch, sample_count, np.nan, np.nan, val])
+
+    def save(self):
+        columns = [
+            'index', 'epoch', 'sample_count',
+            'train_loss', 'val_loss', 'epoch_val_loss']
+        df = pd.DataFrame(self.data, columns=columns)
+        
+        save_path = self.hist_filename if self.save_dir == None else self.save_dir+self.hist_filename
+        df.to_csv(save_path, index=False)
+
+def get_class_names(df):
+    labels = df['class']
+    return labels.unique()
+
+def get_id(filename):
+    # e.g. filename: case123_day20/scans/slice_0001_266_266_1.50_1.50.png
+    # id: case123_day20_slice_0001
+    tokens = filename.split('/')
+    return tokens[-3] + '_' + '_'.join(tokens[-1].split('_')[:2])
+
+# adapted from https://www.kaggle.com/paulorzp/run-length-encode-and-decode
+def rle_decode(mask_rle, shape):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (height,width) of array to return
+    Returns numpy array, 1 - mask, 0 - background
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape)
+
+def get_mask(filename, meta_df, class_names):
+    img_id = get_id(filename)
+    annos = meta_df[meta_df['id'] == img_id].reset_index(drop=True)
+
+    basename = os.path.basename(filename)
+    tokens = basename.split('_')
+    height, width = int(tokens[3]), int(tokens[2])
+    mask = np.zeros((height, width, len(class_names)), dtype=np.uint8)
+    class_map = {name: i for i, name in enumerate(class_names)}
+    for anno_id, row in annos.iterrows():
+        anno = row['segmentation']
+        if pd.isnull(anno):
+            continue
+
+        class_name = row['class']
+        class_id = class_map[class_name]
+        mask[:, :, class_id] = rle_decode(anno, (height, width))
+    return mask
+
+def dice_coeff(labels, preds):
+    return 2*(labels*preds).sum()/(labels.sum() + preds.sum() + 1e-6)
+
+def process_files(input_dir, img_dir, meta_df, class_names):
+    filename = 'train_processed.csv'
+    if os.path.exists(filename):
+        df = pd.read_csv(filename)
+        # delete bad data
+        df = df[~df['img_files'].str.contains('case7_day0') & ~df['img_files'].str.contains('case81_day30')].reset_index(drop=True)
+        return df
+
+    img_files = []
+    subdir = ''
+    while len(img_files) == 0:
+        img_files = sorted(glob(f'{input_dir}/{img_dir}/{subdir}*.png'))
+        subdir += '*/'
+        if len(subdir) > 10:
+            return None
+    '''
+    mask_dir = input_dir+'/masks'
+    pbar = tqdm(enumerate(img_files), total=len(img_files), desc='Maskgen ')
+    for step, img_file in pbar:
+        mask = get_mask(img_file, meta_df, class_names)
+        basename = img_file.replace(f'{input_dir}/{img_dir}/', '')
+        mask_filename = f'{mask_dir}/{basename}'
+        dirname = os.path.dirname(mask_filename)
+        os.makedirs(dirname, exist_ok=True)
+        cv2.imwrite(mask_filename, mask)
+    '''
+    df = pd.DataFrame()
+    # delete common prefix from paths
+    img_files = [f.replace(f'{input_dir}/{img_dir}/', '') for f in img_files]
+    df['img_files'] = img_files
+    df.to_csv(filename, index=False)
+    return df
+
+def visualize(**images):
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        image = image.transpose((1, 2, 0))
+        image -= image.min()
+        max_val = image.max()
+        if max_val != 0:
+            image /= max_val
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(name)
+        plt.imshow(image)
+    plt.show()
+
+def make_test_augmenter(conf):
+    h_crop_size = round(conf.h_image_size*conf.crop_size)
+    w_crop_size = round(conf.w_image_size*conf.crop_size)
+    
+    return  A.Compose([
+        A.CenterCrop(height=h_crop_size, width=w_crop_size),
+        ToTensorV2(transpose_mask=True)
+    ])
